a5b083720358455604f27751c8e92496dd110d0b
Kevin Shoemaker <kevintshoemaker@gmail.com>
2023-10-08 10:55:30 -0700

    minor

--- test.R
+++ test.R
@@ -52,3 +52,66 @@ plot(reps[[1]])
 lapply(coefs,abline,col=rgb(0.4,0.4,0.4,0.4))
 
 
+######
+
+data(mtcars)
+
+df = mtcars[,c("disp","mpg")]
+
+plot(mpg~disp,df)
+
+
+
+negexp <- function(a,b,x){
+  a*exp(b*x)
+}
+
+curve(negexp(35,-0.002,x),add=T)
+
+params <- c(a=35,b=-0.002,sigma=2)
+nll_full <- function(params){
+  -sum(dnorm(df$mpg,negexp(params["a"],params["b"],df$disp),params["sigma"],log=T))
+}
+nll_full(params)
+
+mle <- optim(params,nll_full)
+bestparams <- mle$par
+max_nll <- -mle$value
+
+
+## develop profile likelihood confidence intervals
+
+
+nll_fixsig <- function(params,sigma){
+  -sum(dnorm(df$mpg,negexp(params["a"],params["b"],df$disp),sigma,log=T))
+}
+
+nll_fixa <- function(params,a){
+  -sum(dnorm(df$mpg,negexp(a,params["b"],df$disp),params['sigma'],log=T))
+}
+
+nll_fixb <- function(params,b){
+  -sum(dnorm(df$mpg,negexp(params["a"],b,df$disp),params['sigma'],log=T))
+}
+
+as <- seq(25,40,length=1000)
+bs <- seq(-0.01,-0.0001,length=1000)
+sigs <- seq(1,10,length=1000)
+
+mls_a = sapply(as,function(t) -1*optim(params[setdiff(names(params),"a")],nll_fixa,a=t)$value )
+ci_a = range(as[which(abs(mls_a-max_nll)<(qchisq(0.95,1)/2) )])
+plot(as,mls_a,type="l")
+abline(v=ci_a,lty=2)
+
+mls_b = sapply(bs,function(t) -1*optim(params[setdiff(names(params),"b")],nll_fixb,b=t)$value )
+ci_b = range(bs[which(abs(mls_b-max_nll)<(qchisq(0.95,1)/2) )])
+plot(bs,mls_b,type="l")
+abline(v=ci_b,lty=2)
+
+mls_sig = sapply(sigs,function(t) -1*optim(params[setdiff(names(params),"sigma")],nll_fixsig,sigma=t)$value )
+ci_sig = range(sigs[which(abs(mls_sig-max_nll)<(qchisq(0.95,1)/2) )])
+plot(sigs,mls_sig,type="l")
+abline(v=ci_sig,lty=2)
+
+
+
