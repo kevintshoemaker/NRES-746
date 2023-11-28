4ec283be3f837cf56371affc3f29c412f7fde5ab
Kevin Shoemaker <kevintshoemaker@gmail.com>
2023-11-27 22:39:47 -0800

    Update rfregression_worked.R

--- rfregression_worked.R
+++ rfregression_worked.R
@@ -23,29 +23,29 @@ response <- "ET"
 # generate random variables ------------------
 
 df$random0 <- rnorm(nrow(df))
-df$random10 <- faux::rnorm_pre(df[,response],r=0.1)
-df$random25 <- faux::rnorm_pre(df[,response],r=0.25)
-df$random50 <- faux::rnorm_pre(df[,response],r=0.5)
-
+df$random10 <- faux::rnorm_pre(df[,response],r=0.1) # .1 correlated
+df$random25 <- faux::rnorm_pre(df[,response],r=0.25) # .25 correlated
+df$random50 <- faux::rnorm_pre(df[,response],r=0.5)  # .50 correlated
 
 # separate test and training data ----------------
 
-testndx <- seq(1,nrow(df),5)   # rows to withhold
+testndx <- seq(1,nrow(df),5)   # rows to withhold (every fifth row)
 trainndx <- setdiff(1:nrow(df),testndx)
 
 testdat <- df[testndx,]
 traindat <- df[trainndx,]
 
-
 # tune random forest analysis ---------------
 
-rf.tune <- tuneRF(y = traindat[,response],x = traindat[,predictors],ntreeTry=500,
-       mtryStart = 11,stepFactor = 2,trace = TRUE,plot = TRUE )    # find optimal mtry parameter
+rf.tune <- tuneRF(y = traindat[,response],x = traindat[,predictors],
+                  mtryStart=1,step=0.9,ntreeTry = 100,trace = TRUE,improve=1e-5 )    # find optimal mtry parameter
+
 
 # run random forest analysis -----------------
 
 rf <- randomForest(x = traindat[,predictors],y = traindat[,response],
-                   ntree = 500,mtry = 4,importance = TRUE,proximity = TRUE)
+                   ntree = 500,mtry = 3,replace=T,sampsize = round(0.5*nrow(df)),
+                   importance = TRUE,proximity = TRUE)
 
 rf
 
@@ -56,7 +56,6 @@ plot(rf)  # Plotting OOB error rate
 
 cv <- rf.crossValidation(x= rf, xdata = traindat[,predictors], ydata = traindat[,response], p = 0.1, n = 50)
 summary(cv)
-
 plot(cv, stat = "mse")
 
 cv.v <- rf.crossValidation(x= rf, xdata = testdat[,predictors], ydata = testdat[,response], p = 0.5, n = 25)
@@ -65,8 +64,21 @@ plot(cv.v, stat = "mse")
 summary(cv.v)   # note higher MSE for validation set...
 
 
-#Variable Importance Plot -----------------------
-varImpPlot(rf)
+# Variable Importance Plot -----------------------
+a <- varImpPlot(rf)
+
+# Effects plots -----------------------
+
+ndx <- order(a[,1],decreasing = T)
+predictors <- predictors[ndx]
+
+i=1
+for(i in 1:length(predictors)){
+  partialPlot(rf,pred.data=traindat,x.var=predictors[ndx][i],main=predictors[i])
+}
+
+
+
 
 
 
